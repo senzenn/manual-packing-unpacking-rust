598e6b3b5f9b7517674fb140e7193ac4e9726451
senzenn <ascenzen11@gmail.com>
2025-10-01 15:28:24 +0530

    state check

--- src/state.rs
+++ src/state.rs
@@ -2,8 +2,19 @@ use bytemuck::{Pod, Zeroable};
 
 use solana_program::pubkey::Pubkey;
 
-
 #[repr(C)]
 #[derive(Clone, Copy, Pod, Zeroable)]
 
-pub struct Config
+pub struct Config {
+    pub version: u8,
+    pub admin: Pubkey,
+    pub fee: u64,
+}
+
+impl Config {
+    pub const LEN: usize = 1 + 32 + 8;
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+pub struct UserState {}
